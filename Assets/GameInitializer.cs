d5f1f3a5571b0df34dd7d4d8b21b5e3d5d728c89
Dominik Hirsch <30877193+LittleLegend@users.noreply.github.com>
2018-10-09 13:15:26 +0200

    sort stuff

---
+++ Assets/GameInitializer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameInitializer : MonoBehaviour {
+
+    public References References;
+
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
