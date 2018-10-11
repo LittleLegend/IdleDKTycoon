b3097a251d0425f4cd12091e428a2b3e37a33d89
Dominik Hirsch <30877193+LittleLegend@users.noreply.github.com>
2018-10-11 13:37:33 +0200

    DKCleaning

--- Assets/DonkeyKongView.cs
+++ Assets/DonkeyKongView.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 
 public class DonkeyKongView : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    private Animator _donkeyKongAnimator;
+    
+
+
+    void Start () {
+        _donkeyKongAnimator = gameObject.GetComponent<Animator>();
+
+        _donkeyKongAnimator.SetBool("move", true);
+        _donkeyKongAnimator.SetBool("move", true);
+        _donkeyKongAnimator.SetBool("move", false);
+        _donkeyKongAnimator.SetBool("shoot", false);
+        _donkeyKongAnimator.SetBool("shoot", true);
+
+        //gameObject.transform.position += Vector3.right * Time.deltaTime * _movementSpeed;
+        //var barrelInstance = Instantiate(projectile, new Vector2(transform.position.x, transform.position.y + 0.5f), Quaternion.identity);
+    }
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
--- Assets/Scripts/DonkeyKongController.cs
+++ Assets/Scripts/DonkeyKongController.cs
@@ -9,7 +9,7 @@ using UnityEngine.Experimental.UIElements;
 using UnityEngine.UI;
 using Image = UnityEngine.UI.Image;
 
-public class DonkeyKongController 
+public class DonkeyKongController
 {
 
     private GameObject _donkeyKongPrefab;
@@ -18,13 +18,13 @@ public class DonkeyKongController
     [SerializeField] public float _projectileSpeed;
     [SerializeField] private float _movementSpeed;
     [SerializeField] private float _shotCooldown;
-    
+
     [Space(10)]
 
     private GameObject projectile;
 
 
-    
+
     private float _timeSinceLastShot;
     public bool _collectableRunning;
     private const float MaxOffset = 6.5f;
@@ -37,113 +37,90 @@ public class DonkeyKongController
 
     void Start()
     {
-       
+
         _timeSinceLastShot = _shotCooldown;
-        projectile = InventoryManager.Instance._collectablePrefabDictionary[CollectableEnum.Normal];
+
     }
 
 
     private void GoLeft()
     {
 
-        gameObject.transform.position += Vector3.left * Time.deltaTime * _movementSpeed;
+        
         ClampXPosition();
     }
 
     private void GoRight()
     {
-        gameObject.transform.position += Vector3.right * Time.deltaTime * _movementSpeed;
+        
         ClampXPosition();
     }
 
     private void Fire()
     {
-        DonkeyAnimator.SetBool("shoot", true);
-        var barrelInstance = Instantiate(projectile, new Vector2(transform.position.x, transform.position.y + 0.5f), Quaternion.identity);
+        
+        
 
         SoundEffectService.Instance.PlayClip(ClipIdentifier.DonkeyKong);
     }
 
-    void Update ()
+    void Update()
     {
         if (StateController.IsPaused) return;
 
         if (_timeSinceLastShot < _shotCooldown)
-        { 
-                _timeSinceLastShot += Time.deltaTime;
+        {
+            _timeSinceLastShot += Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
         {
             GoLeft();
-            DonkeyAnimator.SetBool("move", true);
+
         }
         if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
         {
             GoRight();
-            DonkeyAnimator.SetBool("move", true);
+
         }
-        if (!Input.GetKey(KeyCode.RightArrow) && !Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.LeftArrow) && !Input.GetKey(KeyCode.A) )
+        if (!Input.GetKey(KeyCode.RightArrow) && !Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.LeftArrow) && !Input.GetKey(KeyCode.A))
         {
-            DonkeyAnimator.SetBool("move", false);
+
         }
 
-        if (Input.GetKey(KeyCode.X))
-        {
-            if (InventoryManager.Instance.GetActiveSpecialAmount()!=0 && !_collectableRunning)
-            {
-                var obj = InventoryManager.Instance.GetActiveSpecial();
-                if (InventoryManager.Instance._collectableProjectileDictionary[obj])
-                {
-                    projectile = InventoryManager.Instance._collectablePrefabDictionary[obj];
-                   
-                else
-                {
-                    if (obj == CollectableEnum.Daisy)
-                    {
-                      
-                    }
-                    else
-                    {
-                        
-                    }
-                }
-            
-                SoundEffectService.Instance.PlayClip(ClipIdentifier.ItemsActivate);
-                InventoryManager.Instance.RemoveCollectableFromActive();
-               
-            }
-            
-        }
         
+
         if (Input.GetKey(KeyCode.Space))
         {
-            TryFiring();
+               TryFiring();
         }
-        else
-        {
-            DonkeyAnimator.SetBool("shoot", false);
-        }
+            else
+            {
+
+            }
+        
+
+
+        
+
+
+
     }
 
     private void TryFiring()
-    {
-        if (_timeSinceLastShot < _shotCooldown) return;
-        Fire();
-        _timeSinceLastShot = 0f;
-    }
+        {
+            if (_timeSinceLastShot < _shotCooldown) return;
+            Fire();
+            _timeSinceLastShot = 0f;
+        }
 
-    
 
-  
 
-    private void ClampXPosition()
-    {
-        var position = transform.position;
-        position.x = Mathf.Clamp(position.x, -MaxOffset, MaxOffset);
-        transform.position = position;
-    }
 
-    
 
-    
+        private void ClampXPosition()
+        {
+            
+            
+            
+        }
 }
